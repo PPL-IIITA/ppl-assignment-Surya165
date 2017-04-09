16478e70a747dea9b34ac9d7fe18bbbfffad6990
Surya165 <suryakanna165@gmail.com>
2017-04-09 16:41:50 +0545

    Trying to upload the code, hope this works

---
+++ problem5/Problem5.java
@@ -0,0 +1,340 @@
+/*
+
+
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problem5;
+/**
+ *
+ * @author ihm2015004
+ */
+import java.util.Scanner;/**To take input*/
+import java.io.File;/**For file creation*/
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.lang.Object;
+import java.util.Objects;
+/**
+ *
+ * @author iiita
+ */
+public class Problem5 {
+
+	/**
+	 * @param args the command line arguments
+	 * @throws java.io.FileNotFoundException
+	 * @throws java.lang.CloneNotSupportedException
+	 */
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+
+
+		int numberOfGirls,i,n,numberOfBoys,numberOfCouples,c;
+
+		inputCreator input = new inputCreator();
+		File file;
+
+
+
+
+		file = input.getFile(1);
+		Scanner scan = new Scanner(file);
+		n =scan.nextInt();
+		couple[] pair = new couple[n+1];
+
+
+
+
+
+		/*This part of code initiates the girls with the input given in the input file girls.
+		  give input in the following format
+		  name
+		  attraction
+		  intelligence
+		  maintanenceBudget
+		  preference
+		  type
+		 */
+		girl g[] = new girl[n];
+		for(i=0;i<n;i++)
+		{
+			g[i] = new girl();
+			g[i].Name = scan.next();
+			g[i].Attr = scan.nextDouble();
+			g[i].Intel = scan.nextDouble();
+			g[i].Maint = scan.nextDouble();
+			g[i].Pref = scan.next();
+			g[i].Type = scan.next();
+		}
+		numberOfGirls = n;
+
+
+
+
+		/*This part of the code sorts the girls array by maintanance*/
+		girl tempGirl = new girl();
+		for ( i = 0; i < numberOfGirls; i ++ )
+		{
+			for (int j  = i + 1; j < numberOfGirls; j ++ )
+			{
+				if(g[i].Maint > g[j].Maint )
+				{
+					tempGirl = g[i];
+					g[i]= g[j];
+					g[j] = tempGirl;	
+				}
+			}
+		}
+
+
+		/*This part of code initiates the boys with the input given in the files girls
+		  give input in the following format
+		  name
+		  attraction
+		  intelligence
+		  budget
+		  type
+		  minimumAttractionRequired
+		 */
+		file = input.getFile(2);
+		scan = new Scanner(file);
+		n = scan.nextInt();
+		boy[] b = new boy[n];
+		for(i=0;i<n;i++)
+		{
+			b[i] = new boy();
+			b[i].Name = scan.next();
+			b[i].Attr= scan.nextDouble();
+			b[i].Intel = scan.nextDouble();
+			b[i].budget = scan.nextDouble();
+			b[i].Type = scan.next();
+			b[i].MinAttr = scan.nextDouble();
+		}
+		numberOfBoys  = n;
+
+		boy tempBoy = new boy();
+		/*This part of the code sorts the boys list according to attraction*/
+		for ( i = 0; i < numberOfBoys; i ++ )
+		{
+			for ( int j = i + 1; j < numberOfBoys; j ++ )
+			{
+				if ( b[i].Attr > b[j].Attr)
+				{
+					tempBoy = b[i];
+					b[i] = b[j];
+					b[j] = tempBoy;
+				}
+
+			}
+		}
+
+
+
+		/*This part of code initiates all the gifts with input.The input file is taken from the "gifts" file.
+		  Enter the number of gifts first.Then enter each gift in the following format.
+		  type
+		  if type is luxury then
+		  difficulty
+		  luxuryRating
+		  price
+		  value
+		  if type is essential then
+		  price 
+		  value
+		  if type is utility then
+		  price
+		  utilityClass
+		  utilityValue
+		  value
+		 */
+
+
+
+
+
+		file = input.getFile(3);
+		scan = new Scanner (file);
+
+		n = scan.nextInt();
+		gift[] gifts = new gift[n];
+		for ( i = 0; i < n ; i ++ )
+		{
+			gifts[i] = new gift();
+			gifts[i].name = scan.next();
+			gifts[i].type = scan.nextInt();
+			gifts[i].price = scan.nextDouble();
+			/*//this part of code is not neccesary for second question.
+			  if(Objects.equals(gifts[i].type, new String("luxury")))
+			  {
+			  gifts[i].l.difficulty = scan.nextDouble();
+			  gifts[i].l.luxuryRating = scan.nextDouble();
+			  gifts[i].l.price = scan.nextDouble();
+			  gifts[i].l.value = scan.nextDouble();
+			  }
+			  else if ( Objects.equals(gifts[i].type, new String("essential")))
+			  {
+			  gifts[i].e.price = scan.nextDouble();
+			  gifts[i].e.value = scan.nextDouble();
+			  }
+			  else
+			  {
+			  gifts[i].u.price = scan.nextDouble();
+			  gifts[i].u.utilityClass = scan.nextDouble();
+			  gifts[i].u.utilityValue = scan.nextDouble();
+			  gifts[i].u.value = scan.nextDouble();
+			  }*/
+		}
+
+
+
+
+
+		//Sorting Gifts
+		gift temp = new gift();
+		for ( i = 0;i < n; i ++)
+		{
+
+			for(int j = i + 1; j < n; j ++)
+			{
+
+				if (gifts[i].price > gifts[j].price)
+				{
+					temp = gifts[i];
+
+					gifts[i] = gifts[j];
+					gifts[j] = temp;
+				}
+
+			}
+		}
+
+
+
+
+		//For further use, since the boy chooses the most attractive girl Available, it is best to sort the girls according to attraction
+		for ( i = 0; i < numberOfGirls; i ++ )
+		{
+			for ( int j = i + 1; j < numberOfGirls ; j ++ )	
+			{
+				if( g[i].Attr < g[j].Attr )
+				{
+					tempGirl = g[i];
+					g[i] = g[j];
+					g[j] = tempGirl;
+				}
+			}
+		}
+
+
+
+
+
+
+
+		numberOfCouples = 0;
+		/*This part of the code checks all the pairs and initiates a couple if the pair accept each other.
+		  No input is required.
+		  Then it prints the couples so formed.
+		 */
+		for ( i = 0,c = 0; i < g.length; i ++ )
+		{
+
+			if(g[i].check(b[i]) == 1 && i % 2 == 0 ) {
+
+
+				pair[c] = new couple(g[i],b[i]);
+				//				System.out.println(pair[c].b.Name + " " + pair[c].g.Name);
+				pair[c].computeRelationAttributesAndPrintGifts(gifts, gifts.length);
+				c++;
+				g[i].commited = 1;
+			}
+			else
+			{
+				g[i].commited = 0;
+			}
+			if(i % 2 == 1)
+			{
+				for ( int j = 0; j < numberOfGirls;j ++ )
+				{
+					if(g[j].commited == 0 )
+						pair[c] = new couple(g[j],b[i]);
+					g[j].commited = 1;
+				}
+				//pair[c] = new couple(g[i],b[i]);
+				//				System.out.println(pair[c].b.Name + " " + pair[c].g.Name);
+				pair[c].computeRelationAttributesAndPrintGifts(gifts, gifts.length);
+				c++;
+			}
+			numberOfCouples = c;
+		}
+		//      System.out.println(numberOfCouples + "check");
+		for ( i = 0; i < numberOfCouples; i ++)
+		{
+			//	System.out.println(pair[i].b.Name + " " + pair[i].g.Name + "check");
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+		//  System.out.println("At sorting couple happiness");
+		couple tem = new couple(g[0],b[0]);
+
+		for ( i = 0; i < numberOfCouples;i++)
+		{
+
+			for(int j = i + 1 ; j < numberOfCouples; j ++ )
+			{
+
+
+				if(pair[i].happiness < pair[j].happiness);
+				{
+
+					tem = pair[i];
+					pair[i] = pair[j];
+					pair[j] = tem;
+				} 
+
+			}
+		}
+
+
+
+
+
+
+		System.out.println("Enter any number less than "+numberOfCouples);
+		//  System.out.println("At couples most happy");
+		scan = new Scanner(System.in);
+		int k = scan.nextInt();
+
+		//10
+		System.out.println("the "+ k +" most happy couples");
+		if(k < numberOfCouples)
+		{
+			for ( i= 0; i<k; i++) {
+				System.out.println(pair[i].b.Name +" "+pair[i].g.Name);
+			}
+		}
+
+
+
+
+	}
+
+}
+
+
+
+
+
+
---
+++ problem5/inputCreator.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problem5;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+import java.util.Random;
+/**
+ *
+ * @author iiita
+ */
+public class inputCreator {
+    Scanner scan = new Scanner(System.in);
+//    int n = scan.nextInt();
+    String s = System.getProperty("user.dir");
+    int girls;
+    int boys;
+    int k;
+    int gifts;
+    
+    
+    
+    
+    
+    Random r = new Random();
+    
+    
+    File getFile(int i) throws FileNotFoundException, UnsupportedEncodingException
+    {
+       
+        File file;
+        PrintWriter writer;
+        file= new File(s);
+        Scanner input;
+        switch (i) {
+                
+            case 1:
+                input  = new Scanner(System.in);
+                System.out.println("Enter the number of Girls");
+                this.girls = input.nextInt();
+                file = new File(s+"/girls");
+                writer = new PrintWriter(file,"UTF-8");
+                writer.println(this.girls);
+                for( int j = 0; j < this.girls; j ++ )
+                {
+                    writer.println("girl"+j+" " + num(5)  + " " + num(5) + " " + num(5) + " " + num(3) + " " + num(3) );
+                }
+                writer.flush();
+                input = new Scanner(file);
+     
+                break;
+            case 2:
+                input = new Scanner(System.in);
+                System.out.println("Enter the number of boys");
+                this.boys = input.nextInt();       
+     
+                file = new File(s+"/boys");
+                writer = new PrintWriter(file,"UTF-8");
+                writer.println(this.boys);
+                for( int j = 0; j < boys; j ++ )
+                {
+                  writer.println("boy"+j+ " " + num(5) + " " + num(5) + " " + num(10000) + " " + num(3) + " " + num(5)  );
+                }
+                writer.flush();
+                input  = new Scanner(file);
+           
+      
+                break;
+            case 4:
+                //
+            default:
+               
+              input = new Scanner(System.in);
+                System.out.println("Enter the number of gifts:");
+               this.gifts = input.nextInt();       
+
+                file = new File(s+"/gifts");
+               
+                
+                writer = new PrintWriter(file,"UTF-8");
+                writer.println(this.gifts);
+                for( int j = 0; j < this.gifts; j ++ )
+                {
+                  writer.println("gift" + j + " " + num(3) +" " +  num(10000) );
+                }
+                writer.flush();
+                input  = new Scanner(file);
+                break;
+        }
+       
+        return file;
+    }
+    
+    
+    
+    
+    int  num(int j )
+    {
+        int i;
+        i = r.nextInt();
+        i = i % j;
+        i = Math.abs(i);
+        return i;
+    }
+    
+}
